7bf9e669be76d062e4ac3650e4865ae2d76074c0
pedromarsan06 <pedromarsan06@gmail.com>
2025-04-18 22:41:02 -0300

    Update CorePad.h

--- Include/CorePad.h
+++ Include/CorePad.h
@@ -1,44 +1,40 @@
-#ifndef Corepad_H
-#define Corepad_H
+#ifndef corepad_h
+#define corepad_h
 #include <iostream>
 #include <string>
 #include <fstream>
 std::string text;
 std::fstream arquivo;
 void make_file(){
-    std::cout << "List name for your file : \n" << std::endl;
+    std::cout << "List name for your file : \n";
     std::cin >> text;
     std::string comando = "type nul > " + text;
     system(comando.c_str());
 }
 std::string value;
-std::string form;
 void select_file_add(){
-    std::cout << "list your file to acesse \n" << std::endl;
-    std::cin >> form;
-    arquivo.open(form);
-    std::cout << "List your text for your file : \n" << std::endl;
+    arquivo.open(text);
     getline(std::cin, value);
     arquivo << value;
     arquivo.close();
 }
 std::string dir;
 void return_file(){
-    std::cout << "enter your file for view : \n" << std::endl;
+    std::cout << "enter your file for view : \n";
     std::cin >> dir;
     system(dir.c_str());
 }
 std::string rm_file;
 void remove_file(){
-    std::cout << "enter your name file to remove : \n" << std::endl;
+    std::cout << "enter your name file to remove : \n";
     std::cin >> rm_file;
     std::string value_rm = "del " + rm_file;
     system(value_rm.c_str());
 }
 void Core_version(){
-    std::cout << "\n Version: {1.0}, Last-Varsion: {NoN} \n" << std::endl;
+    std::cout << "\n Version: {1.0}, Last-Varsion: {NoN} \n";
 }
 void Core_comands(){
-    std::cout << "\n { < Core_version(), remove_file(), select_file_add(), make_file() > Now-Version-comands: [1,0] } \n" << std::endl;
+    std::cout << "\n { < Core_version(), remove_file(), select_file_add(), make_file() > Now-Version-comands: [1,0] } \n";
 }
 #endif
